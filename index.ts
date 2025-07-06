e66b86abbde6407f84b030063c5e23ee68d37e91
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2025-07-06 23:24:58 +0330

    @haajilari: create user added.

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 // index.ts
-
 import { PrismaClient } from "./generated/prisma";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log(`آماده برای اجرای کوئری با TypeScript...`);
+  console.log(`در حال ساخت یک کاربر جدید...`);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email: "ali.ahmadi@example.com",
+      name: "Ali Ahmadi",
+    },
+  });
+
+  console.log("کاربر جدید با موفقیت ساخته شد:");
+  console.log(newUser);
 }
 
 main()
