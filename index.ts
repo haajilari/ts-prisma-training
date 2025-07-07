9208a6c90f8324b1a5acb6dedad75fd829a3cca3
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2025-07-07 23:22:24 +0330

    @haajilari: update added.

--- index.ts
+++ index.ts
@@ -4,21 +4,34 @@ import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-  // 1. پیدا کردن همه کاربران
-  console.log("در حال دریافت لیست همه کاربران...");
-  const allUsers = await prisma.user.findMany();
-  console.log("همه کاربران:");
-  console.dir(allUsers, { depth: null }); // .dir برای نمایش بهتر آبجکت‌ها
+  console.log("در حال آپدیت کردن نام یک کاربر...");
 
-  // 2. پیدا کردن یک کاربر خاص
-  console.log("\nدر حال جستجوی یک کاربر با ایمیل مشخص...");
-  const specificUser = await prisma.user.findUnique({
+  const updatedUser = await prisma.user.update({
     where: {
-      email: "ali.ahmadi@example.com", // ایمیل کاربری که قبلا ساختید
+      email: "ali.ahmadi@example.com", // کاربری که می‌خواهیم آپدیت کنیم
+    },
+    data: {
+      name: "Ali Ahmadi (Updated Name)", // مقدار جدید برای فیلد name
     },
   });
-  console.log("کاربر پیدا شده:");
-  console.log(specificUser);
+
+  console.log("کاربر با موفقیت آپدیت شد:");
+  console.log(updatedUser);
+  // // 1. پیدا کردن همه کاربران
+  // console.log("در حال دریافت لیست همه کاربران...");
+  // const allUsers = await prisma.user.findMany();
+  // console.log("همه کاربران:");
+  // console.dir(allUsers, { depth: null }); // .dir برای نمایش بهتر آبجکت‌ها
+
+  // // 2. پیدا کردن یک کاربر خاص
+  // console.log("\nدر حال جستجوی یک کاربر با ایمیل مشخص...");
+  // const specificUser = await prisma.user.findUnique({
+  //   where: {
+  //     email: "ali.ahmadi@example.com", // ایمیل کاربری که قبلا ساختید
+  //   },
+  // });
+  // console.log("کاربر پیدا شده:");
+  // console.log(specificUser);
   // console.log(`در حال ساخت یک کاربر جدید...`);
 
   // const newUser = await prisma.user.create({
